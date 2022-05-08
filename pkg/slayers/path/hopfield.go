065103813636c29eed5122c9965f6ccc501ece57
João Pereira <joaopereira.19@gmail.com>
2022-05-08 15:15:12 +0200

    Add spec to 'pkg/slayers/path'

--- pkg/slayers/path/hopfield.go
+++ pkg/slayers/path/hopfield.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +gobra
+
 package path
 
 import (
@@ -70,22 +72,36 @@ type HopField struct {
 
 // DecodeFromBytes populates the fields from a raw buffer. The buffer must be of length >=
 // path.HopLen.
-func (h *HopField) DecodeFromBytes(raw []byte) error {
+//@ requires  len(raw) >= HopLen
+//@ preserves acc(h) && acc(raw, 1/2)
+//@ ensures   err == nil
+//@ decreases
+func (h *HopField) DecodeFromBytes(raw []byte) (err error) {
 	if len(raw) < HopLen {
 		return serrors.New("HopField raw too short", "expected", HopLen, "actual", len(raw))
 	}
 	h.EgressRouterAlert = raw[0]&0x1 == 0x1
 	h.IngressRouterAlert = raw[0]&0x2 == 0x2
 	h.ExpTime = raw[1]
+	//@ assert &raw[2:4][0] == &raw[2] && &raw[2:4][1] == &raw[3]
 	h.ConsIngress = binary.BigEndian.Uint16(raw[2:4])
+	//@ assert &raw[4:6][0] == &raw[4] && &raw[4:6][1] == &raw[5]
 	h.ConsEgress = binary.BigEndian.Uint16(raw[4:6])
-	copy(h.Mac[:], raw[6:6+MacLen])
+	//@ assert forall i int :: { &h.Mac[:][i] } 0 <= i && i < len(h.Mac[:]) ==>
+	//@     &h.Mac[i] == &h.Mac[:][i]
+	//@ assert forall i int :: 0 <= i && i < len(raw[6:6+MacLen]) ==>
+	//@     &raw[6:6+MacLen][i] == &raw[i+6]
+	copy(h.Mac[:], raw[6:6+MacLen] /*@, perm(1/2)@*/)
 	return nil
 }
 
 // SerializeTo writes the fields into the provided buffer. The buffer must be of length >=
 // path.HopLen.
-func (h *HopField) SerializeTo(b []byte) error {
+//@ requires  len(b) >= HopLen
+//@ preserves acc(h, 1/2) && acc(b)
+//@ ensures   err == nil
+//@ decreases
+func (h *HopField) SerializeTo(b []byte) (err error) {
 	if len(b) < HopLen {
 		return serrors.New("buffer for HopField too short", "expected", MacLen, "actual", len(b))
 	}
@@ -97,15 +113,22 @@ func (h *HopField) SerializeTo(b []byte) error {
 		b[0] |= 0x2
 	}
 	b[1] = h.ExpTime
+	//@ assert &b[2:4][0] == &b[2] && &b[2:4][1] == &b[3]
 	binary.BigEndian.PutUint16(b[2:4], h.ConsIngress)
+	//@ assert &b[4:6][0] == &b[4] && &b[4:6][1] == &b[5]
 	binary.BigEndian.PutUint16(b[4:6], h.ConsEgress)
-	copy(b[6:6+MacLen], h.Mac[:])
+	//@ assert forall i int :: { &h.Mac[:][i] } 0 <= i && i < len(h.Mac) ==>
+	//@     &h.Mac[i] == &h.Mac[:][i]
+	//@ assert forall i int :: 0 <= i && i < MacLen ==>
+	//@     &b[6:6+MacLen][i] == &b[i+6]
+	copy(b[6:6+MacLen], h.Mac[:] /*@, perm(1/2) @*/)
 
 	return nil
 }
 
 // ExpTimeToDuration calculates the relative expiration time in seconds.
 // Note that for a 0 value ExpTime, the minimal duration is expTimeUnit.
+//@ decreases
 func ExpTimeToDuration(expTime uint8) time.Duration {
 	return (time.Duration(expTime) + 1) * time.Duration(expTimeUnit) * time.Second
 }
--- pkg/slayers/path/infofield.go
+++ pkg/slayers/path/infofield.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +gobra
+
 package path
 
 import (
@@ -55,20 +57,31 @@ type InfoField struct {
 
 // DecodeFromBytes populates the fields from a raw buffer. The buffer must be of length >=
 // path.InfoLen.
-func (inf *InfoField) DecodeFromBytes(raw []byte) error {
+//@ requires  len(raw) >= InfoLen
+//@ preserves acc(inf) && acc(raw, 1/2)
+//@ ensures   err == nil
+//@ decreases
+func (inf *InfoField) DecodeFromBytes(raw []byte) (err error) {
 	if len(raw) < InfoLen {
 		return serrors.New("InfoField raw too short", "expected", InfoLen, "actual", len(raw))
 	}
 	inf.ConsDir = raw[0]&0x1 == 0x1
 	inf.Peer = raw[0]&0x2 == 0x2
+	//@ assert &raw[2:4][0] == &raw[2] && &raw[2:4][1] == &raw[3]
 	inf.SegID = binary.BigEndian.Uint16(raw[2:4])
+	//@ assert &raw[4:8][0] == &raw[4] && &raw[4:8][1] == &raw[5]
+	//@ assert &raw[4:8][2] == &raw[6] && &raw[4:8][3] == &raw[7]
 	inf.Timestamp = binary.BigEndian.Uint32(raw[4:8])
 	return nil
 }
 
 // SerializeTo writes the fields into the provided buffer. The buffer must be of length >=
 // path.InfoLen.
-func (inf *InfoField) SerializeTo(b []byte) error {
+//@ requires  len(b) >= InfoLen
+//@ preserves acc(inf, 1/2)
+//@ preserves acc(b)
+//@ ensures   err == nil
+func (inf *InfoField) SerializeTo(b []byte) (err error) {
 	if len(b) < InfoLen {
 		return serrors.New("buffer for InfoField too short", "expected", InfoLen,
 			"actual", len(b))
@@ -81,7 +94,10 @@ func (inf *InfoField) SerializeTo(b []byte) error {
 		b[0] |= 0x2
 	}
 	b[1] = 0 // reserved
+	//@ assert &b[2:4][0] == &b[2] && &b[2:4][1] == &b[3]
 	binary.BigEndian.PutUint16(b[2:4], inf.SegID)
+	//@ assert &b[4:8][0] == &b[4] && &b[4:8][1] == &b[5]
+	//@ assert &b[4:8][2] == &b[6] && &b[4:8][3] == &b[7]
 	binary.BigEndian.PutUint32(b[4:8], inf.Timestamp)
 
 	return nil
@@ -90,10 +106,15 @@ func (inf *InfoField) SerializeTo(b []byte) error {
 // UpdateSegID updates the SegID field by XORing the SegID field with the 2
 // first bytes of the MAC. It is the beta calculation according to
 // https://scion.docs.anapaya.net/en/latest/protocols/scion-header.html#hop-field-mac-computation
+//@ trusted // reason: https://github.com/viperproject/gobra/issues/435
+//@ preserves acc(inf)
+//@ decreases
 func (inf *InfoField) UpdateSegID(hfMac [MacLen]byte) {
 	inf.SegID = inf.SegID ^ binary.BigEndian.Uint16(hfMac[:2])
 }
 
+//@ trusted // reason: https://github.com/viperproject/gobra/issues/257
+//@ decreases
 func (inf InfoField) String() string {
 	return fmt.Sprintf("{Peer: %t, ConsDir: %t, SegID: %d, Timestamp: %s}",
 		inf.Peer, inf.ConsDir, inf.SegID, util.SecsToCompact(inf.Timestamp))
