15bf7db4ba3f37226464893638beb0d21183df17
João Pereira <joaopereira.19@gmail.com>
2022-05-09 17:34:24 +0200

    Verify function that was trusted

--- pkg/slayers/path/infofield.go
+++ pkg/slayers/path/infofield.go
@@ -106,10 +106,10 @@ func (inf *InfoField) SerializeTo(b []byte) (err error) {
 // UpdateSegID updates the SegID field by XORing the SegID field with the 2
 // first bytes of the MAC. It is the beta calculation according to
 // https://scion.docs.anapaya.net/en/latest/protocols/scion-header.html#hop-field-mac-computation
-//@ trusted // reason: https://github.com/viperproject/gobra/issues/435
 //@ preserves acc(inf)
 //@ decreases
 func (inf *InfoField) UpdateSegID(hfMac [MacLen]byte) {
+	//@ share hfMac
 	inf.SegID = inf.SegID ^ binary.BigEndian.Uint16(hfMac[:2])
 }
 
