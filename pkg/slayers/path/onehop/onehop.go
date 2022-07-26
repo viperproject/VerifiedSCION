69d430c4b9940dc36e93910a313cb50444846574
gavinleroy <gavinleroy6@gmail.com>
2022-07-26 06:52:01 -0600

    path/onehop

--- pkg/slayers/path/onehop/onehop.go
+++ pkg/slayers/path/onehop/onehop.go
@@ -21,6 +21,7 @@ import (
 	"github.com/scionproto/scion/pkg/slayers/path"
 	"github.com/scionproto/scion/pkg/slayers/path/scion"
 	//@ "github.com/scionproto/scion/verification/utils/definitions"
+	//@ "github.com/scionproto/scion/verification/utils/slices"
 )
 
 // PathLen is the length of a serialized one hop path in bytes.
@@ -47,13 +48,11 @@ type Path struct {
 	SecondHop path.HopField
 }
 
-//@ trusted
-//@ requires o.NonInitMem()
-//@ requires len(data) >= PathLen
-//@ preserves forall i int :: 0 <= i && i < len(data) ==>
-//@     acc(&data[i], definitions.ReadL1)
-//@ ensures r != nil ==> (o.NonInitMem() && r.ErrorMem())
-//@ ensures r == nil ==> o.Mem()
+//@ requires  o.NonInitMem()
+//@ requires  len(data) >= PathLen
+//@ preserves acc(slices.AbsSlice_Bytes(data, 0, len(data)), definitions.ReadL1)
+//@ ensures   r != nil ==> (o.NonInitMem() && r.ErrorMem())
+//@ ensures   r == nil ==> o.Mem()
 //@ decreases
 func (o *Path) DecodeFromBytes(data []byte) (r error) {
 	if len(data) < PathLen {
@@ -62,32 +61,45 @@ func (o *Path) DecodeFromBytes(data []byte) (r error) {
 	}
 	offset := 0
 	//@ unfold o.NonInitMem()
+	//@ ghost slices.SplitByIndex_Bytes(data, 0, len(data), path.InfoLen, definitions.ReadL1)
+	//@ ghost slices.Reslice_Bytes(data, 0, path.InfoLen, definitions.ReadL1)
 	if err := o.Info.DecodeFromBytes(data[:path.InfoLen]); err != nil {
+		//@ ghost slices.Unslice_Bytes(data, 0, path.InfoLen, definitions.ReadL1)
+		//@ ghost slices.CombineAtIndex_Bytes(data, 0, len(data), path.InfoLen, definitions.ReadL1)
 		return err
 	}
+	//@ slices.Unslice_Bytes(data, 0, path.InfoLen, definitions.ReadL1)
 	offset += path.InfoLen
-	//@ assert 0 <= offset && offset + path.HopLen <= len(data)
-	//@ assert forall i int :: 0 <= i && i < path.HopLen ==>
-	//@     &data[offset : offset+path.HopLen][i] == &data[offset + i]
+	//@ assert acc(slices.AbsSlice_Bytes(data, 0, offset), definitions.ReadL1)
+	//@ ghost slices.SplitByIndex_Bytes(data, offset, len(data), offset+path.HopLen, definitions.ReadL1)
+	//@ ghost slices.Reslice_Bytes(data, offset, offset+path.HopLen, definitions.ReadL1)
 	if err := o.FirstHop.DecodeFromBytes(data[offset : offset+path.HopLen]); err != nil {
+		//@ ghost slices.Unslice_Bytes(data, offset, offset+path.HopLen, definitions.ReadL1)
+		//@ ghost slices.CombineAtIndex_Bytes(data, offset, len(data), offset+path.HopLen, definitions.ReadL1)
+		//@ ghost slices.CombineAtIndex_Bytes(data, 0, len(data), offset, definitions.ReadL1)
 		return err
 	}
+	//@ ghost slices.Unslice_Bytes(data, offset, offset+path.HopLen, definitions.ReadL1)
+	//@ ghost slices.CombineAtIndex_Bytes(data, 0, offset+path.HopLen, offset, definitions.ReadL1)
 	offset += path.HopLen
-	//@ assert 0 <= offset && offset + path.HopLen <= len(data)
-	//@ assert forall i int :: 0 <= i && i < path.HopLen ==>
-	//@     &data[offset : offset+path.HopLen][i] == &data[offset + i]
+	//@ ghost slices.SplitByIndex_Bytes(data, offset, len(data), offset+path.HopLen, definitions.ReadL1)
+	//@ ghost slices.Reslice_Bytes(data, offset, offset+path.HopLen, definitions.ReadL1)
 	r = o.SecondHop.DecodeFromBytes(data[offset : offset+path.HopLen])
+	//@ ghost slices.Unslice_Bytes(data, offset, offset+path.HopLen, definitions.ReadL1)
+	//@ ghost slices.CombineAtIndex_Bytes(data, offset, len(data), offset+path.HopLen, definitions.ReadL1)
+	//@ ghost slices.CombineAtIndex_Bytes(data, 0, len(data), offset, definitions.ReadL1)
 	//@ ghost if r == nil {
-	//@     fold o.Mem()
+	//@   fold o.Mem()
+	//@ } else {
+	//@   fold o.NonInitMem()
 	//@ }
 	return r
 }
 
-//@ requires len(b) >= PathLen
+//@ requires  len(b) >= PathLen
 //@ preserves acc(o.Mem(), definitions.ReadL1)
-//@ preserves forall i int :: 0 <= i && i < len(b) ==>
-//@     acc(&b[i])
-//@ ensures err == nil
+//@ preserves slices.AbsSlice_Bytes(b, 0, len(b))
+//@ ensures   err == nil
 //@ decreases
 func (o *Path) SerializeTo(b []byte) (err error) {
 	if len(b) < PathLen {
@@ -96,32 +108,41 @@ func (o *Path) SerializeTo(b []byte) (err error) {
 	}
 	offset := 0
 	//@ unfold acc(o.Mem(), definitions.ReadL1)
+	//@ ghost slices.SplitByIndex_Bytes(b, 0, len(b), path.InfoLen, writePerm)
+	//@ ghost slices.Reslice_Bytes(b, 0, path.InfoLen, writePerm)
 	if err := o.Info.SerializeTo(b[:offset+path.InfoLen]); err != nil {
+		//@ ghost slices.Unslice_Bytes(b, 0, path.InfoLen, writePerm)
+		//@ ghost slices.CombineAtIndex_Bytes(b, 0, len(b), path.InfoLen, writePerm)
 		return err
 	}
+	//@ ghost slices.Unslice_Bytes(b, 0, path.InfoLen, writePerm)
 	offset += path.InfoLen
-	//@ assert 0 <= offset && offset + path.HopLen <= len(b)
-	//@ assert forall i int :: 0 <= i && i < path.HopLen ==>
-	//@     &b[offset : offset+path.HopLen][i] == &b[offset + i]
+	//@ ghost slices.SplitByIndex_Bytes(b, offset, len(b), offset+path.HopLen, writePerm)
+	//@ ghost slices.Reslice_Bytes(b, offset, offset+path.HopLen, writePerm)
 	if err := o.FirstHop.SerializeTo(b[offset : offset+path.HopLen]); err != nil {
+		//@ ghost slices.Unslice_Bytes(b, offset, offset+path.HopLen, writePerm)
+		//@ ghost slices.CombineAtIndex_Bytes(b, offset, len(b), offset+path.HopLen, writePerm)
+		//@ ghost slices.CombineAtIndex_Bytes(b, 0, len(b), offset, writePerm)
 		return err
 	}
+	//@ ghost slices.Unslice_Bytes(b, offset, offset+path.HopLen, writePerm)
+	//@ ghost slices.CombineAtIndex_Bytes(b, 0, offset+path.HopLen, offset, writePerm)
 	offset += path.HopLen
-	//@ assert 0 <= offset && offset + path.HopLen <= len(b)
-	//@ assert forall i int :: 0 <= i && i < path.HopLen ==>
-	//@     &b[offset : offset+path.HopLen][i] == &b[offset + i]
+	//@ ghost slices.SplitByIndex_Bytes(b, offset, len(b), offset+path.HopLen, writePerm)
+	//@ ghost slices.Reslice_Bytes(b, offset, offset+path.HopLen, writePerm)
 	err = o.SecondHop.SerializeTo(b[offset : offset+path.HopLen])
-	//@ ghost if err == nil {
-	//@   fold acc(o.Mem(), definitions.ReadL1)
-	//@ }
+	//@ ghost slices.Unslice_Bytes(b, offset, offset+path.HopLen, writePerm)
+	//@ ghost slices.CombineAtIndex_Bytes(b, offset, len(b), offset+path.HopLen, writePerm)
+	//@ ghost slices.CombineAtIndex_Bytes(b, 0, len(b), offset, writePerm)
+	//@ fold acc(o.Mem(), definitions.ReadL1)
 	return err
 }
 
 // ToSCIONDecoded converts the one hop path in to a normal SCION path in the
 // decoded format.
 //@ requires o.Mem()
-//@ ensures err == nil ==> sd.Mem()
-//@ ensures err != nil ==> o.Mem()
+//@ ensures  err == nil ==> sd.Mem()
+//@ ensures  err != nil ==> o.Mem()
 //@ decreases
 func (o *Path) ToSCIONDecoded() (sd *scion.Decoded, err error) {
 	//@ unfold o.Mem()
@@ -166,19 +187,24 @@ func (o *Path) ToSCIONDecoded() (sd *scion.Decoded, err error) {
 			},
 		},
 	}
+	// TODO (gavin) this verification times out. Even folding
+	// the base predicate and assuming false for the rest takes a
+	// significant amount of time.
 	//@ fold p.Base.Mem()
+	//@ fold p.HopFields[0].Mem()
+	//@ fold p.HopFields[1].Mem()
 	//@ fold p.Mem()
 	return p, nil
 }
 
 // Reverse a OneHop path that returns a reversed SCION path.
 //@ trusted // TODO
+//@ ensures err == nil ==> p.Mem()
 //@ decreases
 func (o /*@@@*/ Path) Reverse() (p path.Path, err error) {
 	//@ fold o.FirstHop.Mem()
 	//@ fold o.SecondHop.Mem()
 	//@ fold o.Mem()
-	// (gavin) changed to use explicit *Path
 	sp, err := o.ToSCIONDecoded()
 	if err != nil {
 		return nil, serrors.WrapStr("converting to scion path", err)
