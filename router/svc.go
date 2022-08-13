f4bcf3c75de0b582a12f316513b1b5ca6a1b06bd
João Pereira <joaopereira.19@gmail.com>
2022-08-14 00:17:57 +0200

    Major simplification, sidestep injectivity checks

--- router/svc.go
+++ router/svc.go
@@ -40,8 +40,6 @@ func newServices() (s *services) {
 	return tmp
 }
 
-// WIP
-//@ trusted
 //@ requires acc(s.Mem(), _)
 //@ requires acc(a.Mem())
 func (s *services) AddSvc(svc addr.HostSVC, a *net.UDPAddr) {
@@ -51,27 +49,20 @@ func (s *services) AddSvc(svc addr.HostSVC, a *net.UDPAddr) {
 
 	//@ unfold internalLockInv!<s!>()
 	addrs := s.m[svc]
-	/*@
-	ghost if addrs == nil {
-		fold validMapValue(addrs)
-	}
-	@*/
-	//@ unfold acc(validMapValue(addrs), definitions.ReadL10)
-	if _, ok := s.index(a, addrs); ok {
+	//@ ghost if addrs == nil { fold validMapValue(svc, addrs) }
+	//@ unfold acc(validMapValue(svc, addrs), definitions.ReadL10)
+	if _, ok := s.index(a, addrs /*@, svc @*/); ok {
 		//@ fold internalLockInv!<s!>()
 		return
 	}
-	//@ unfold acc(validMapValue(addrs), definitions.ReadL10)
+	//@ unfold acc(validMapValue(svc, addrs), definitions.ReadL10)
 	s.m[svc] = append( /*@ definitions.ReadL10, @*/ addrs, a)
-	//@ assert forall i, j int :: { &addrs[i], &addrs[j] } 0 <= i && i < len(addrs) ==>
-	//@	(0 <= j && j < len(addrs) ==>
-	//@		(i != j ==> !equalUDPAddr(addrs[i], addrs[j])))
-	//@ fold validMapValue(s.m[svc])
+	//@ ghost tmp := s.m[svc]
+	//@ fold InjectiveMem(tmp[len(tmp)-1], len(tmp)-1)
+	//@ fold validMapValue(svc, s.m[svc])
 	//@ fold internalLockInv!<s!>()
 }
 
-// WIP
-//@ trusted
 //@ requires  acc(s.Mem(), _)
 //@ preserves acc(a.Mem(), definitions.ReadL10)
 func (s *services) DelSvc(svc addr.HostSVC, a *net.UDPAddr) {
@@ -81,26 +72,27 @@ func (s *services) DelSvc(svc addr.HostSVC, a *net.UDPAddr) {
 
 	//@ unfold internalLockInv!<s!>()
 	addrs := s.m[svc]
-	//@ ghost if addrs == nil { fold validMapValue(addrs) }
-	//@ assert validMapValue(addrs)
-	index, ok := s.index(a, addrs)
+	//@ ghost if addrs == nil { fold validMapValue(svc, addrs) }
+	//@ assert validMapValue(svc, addrs)
+	index, ok := s.index(a, addrs /*@, svc @*/)
 	if !ok {
 		//@ fold internalLockInv!<s!>()
 		return
 	}
 	//@ assert 0 < len(addrs)
-	//@ unfold validMapValue(addrs)
+	//@ unfold validMapValue(svc, addrs)
+	//@ unfold InjectiveMem(addrs[len(addrs)-1], len(addrs)-1)
 	addrs[index] = addrs[len(addrs)-1]
+	//@ fold InjectiveMem(addrs[index], index)
 	addrs[len(addrs)-1] = nil
 	//@ assert forall i int :: 0 <= i && i < len(addrs)-1 ==> &addrs[:len(addrs)-1][i] == &addrs[i]
 	s.m[svc] = addrs[:len(addrs)-1]
-	//@ assert forall h addr.HostSVC :: h != svc ==> s.m[h] != s.m[svc]
+	//@ fold validMapValue(svc, s.m[svc])
 	//@ fold internalLockInv!<s!>()
 }
 
-// WIP
 //@ requires acc(s.Mem(), _)
-//@ ensures !b ==> r == nil
+//@ ensures  !b ==> r == nil
 //@ ensures  b  ==> acc(r.Mem(), _)
 func (s *services) Any(svc addr.HostSVC) (r *net.UDPAddr, b bool) {
 	//@ unfold acc(s.Mem(), _)
@@ -117,36 +109,43 @@ func (s *services) Any(svc addr.HostSVC) (r *net.UDPAddr, b bool) {
 	//@ unfold acc(validMapValue(svc, addrs))
 	//@ defer fold internalLockInv!<s!>()
 	//@ defer fold acc(validMapValue(svc, addrs))
-	return addrs[mrand.Intn(len(addrs))], true
+	tmpIdx := mrand.Intn(len(addrs))
+	tmpAddr := addrs[tmpIdx]
+	//@ unfold InjectiveMem(tmpAddr, tmpIdx)
+	//@ fold InjectiveMem(tmpAddr, tmpIdx)
+	return tmpAddr, true
 }
 
-// Verified but marked as trusted to improve verification time.
-//@ trusted
 //@ preserves acc(a.Mem(), definitions.ReadL10)
 //@ preserves acc(validMapValue(k, addrs), definitions.ReadL10)
 //@ ensures   b ==> res >= 0 && 0 < len(addrs) && 0 <= res && res < len(addrs)
 //@ ensures   b ==> 0 < len(addrs)
 //@ ensures   b ==> 0 <= res && res < len(addrs)
-// The following postcondition cannot be shown to hold even though it does
-// ensures   b ==> unfolding acc(validMapValue(addrs), definitions.ReadL10) in equalUDPAddr(addrs[res], a)
 //@ ensures   !b ==> res == -1
+// We could ensure stronger postconditions for this method,
+// but it is unclear right now if we need them.
 //@ decreases
 func (s *services) index(a *net.UDPAddr, addrs []*net.UDPAddr /*@ , ghost k addr.HostSVC @*/) (res int, b bool) {
-	//@ unfold acc(validMapValue(addrs), definitions.ReadL11)
-	//@ defer  fold acc(validMapValue(addrs), definitions.ReadL11)
+	//@ unfold acc(validMapValue(k, addrs), definitions.ReadL11)
+	//@ defer  fold acc(validMapValue(k, addrs), definitions.ReadL11)
 
 	//@ invariant acc(a.Mem(), definitions.ReadL10)
-	//@ invariant (forall i1 int :: 0 <= i1 && i1 < len(addrs) ==> (forall j int :: 0 <= j && j < len(addrs) ==> (i1 != j ==> &addrs[i1] != &addrs[j])))
 	//@ invariant acc(addrs, definitions.ReadL11)
-	//@ invariant forall u *net.UDPAddr :: u in sliceToSet(addrs) ==> acc(u.Mem(), _)
+	// Right now, Gobra seems to mix quantified variables `i`
+	// with the declared variable i.
+	// That is why we are using `i1`.
+	// Is this a problem with how we handle range loops?
+	//@ invariant forall i1 int :: 0 <= i1 && i1 < len(addrs) ==> acc(InjectiveMem(addrs[i1], i1), definitions.ReadL11)
 	//@ decreases len(addrs) - i
 	for i, o := range addrs {
-		// TODO: Gobra currently cannot prove that the loop is non-empty at this
-		//       point, even though it must definitely not be. This assume deals
-		//       with that Gobra limitation.
+		// TODO: Gobra currently cannot prove that the iterated slice
+		// is non-empty at this point, even though it must definitely
+		// not be. Is that a bug with range statements?
+		// This temporary assume deals with that Gobra limitation.
 		//@ assume len(addrs) > 0
 		//@ unfold acc(a.Mem(), definitions.ReadL10)
-		//@ assert o in sliceToSet(addrs)
+		//@ unfold acc(InjectiveMem(addrs[i], i), definitions.ReadL11)
+		//@ fold acc(InjectiveMem(addrs[i], i), definitions.ReadL11)
 		//@ unfold acc(o.Mem(), _)
 		if a.IP.Equal(o.IP) && a.Port == o.Port {
 			//@ fold acc(a.Mem(), definitions.ReadL10)
