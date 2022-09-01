7b1e95a3eeda57e71de6796ff3e8b43e4cbdb5dc
gavinleroy <gavinleroy6@gmail.com>
2022-09-01 11:33:10 +0200

    Missing 'gobra' annotation for scion.go

--- pkg/slayers/scion.go
+++ pkg/slayers/scion.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +gobra
+
 //@ initEnsures path.PathPackageMem()
 //@ initEnsures path.Registered(empty.PathType)
 //@ initEnsures path.Registered(epic.PathType)
