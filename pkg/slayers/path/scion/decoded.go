273d08d034e9841b53b0a7a3156c02d08c05dc24
gavinleroy <gavinleroy6@gmail.com>
2022-09-21 22:30:21 +0200

    Removed data length assumption scion.Decoded

--- pkg/slayers/path/scion/decoded.go
+++ pkg/slayers/path/scion/decoded.go
@@ -171,14 +171,16 @@ func (s *Decoded) SerializeTo(b []byte /*@, buf []byte, dataLen int @*/) (r erro
 	//@ assert slices.AbsSlice_Bytes(b, 0, len(b))
 	//@ unfold acc(s.NoBufMem(buf), definitions.ReadL1)
 	//@ unfold acc(s.Base.Mem(), definitions.ReadL1)
+	//@ assert len(b) >= MetaLen + s.NumINF * path.InfoLen + s.NumHops * path.HopLen
 	//@ ghost slices.SplitByIndex_Bytes(b, 0, len(b), MetaLen, writePerm)
-	//@ requires  slices.AbsSlice_Bytes(b, 0, MetaLen)
-	//@ preserves MetaLen <= len(b)
-	//@ ensures   slices.AbsSlice_Bytes(b[:MetaLen], 0, len(b[:MetaLen]))
-	//@ decreases
-	//@ outline(
+	// requires  slices.AbsSlice_Bytes(b, 0, MetaLen)
+	// preserves MetaLen <= len(b)
+	// ensures   len(b) == before(len(b))
+	// ensures   slices.AbsSlice_Bytes(b[:MetaLen], 0, len(b[:MetaLen]))
+	// decreases
+	// outline(
 	//@ ghost slices.Reslice_Bytes(b, 0, MetaLen, writePerm)
-	//@ )
+	// )
 	if err := s.PathMeta.SerializeTo(b[:MetaLen]); err != nil {
 		//@ ghost slices.Unslice_Bytes(b, 0, MetaLen, writePerm)
 		//@ ghost slices.CombineAtIndex_Bytes(b, 0, len(b), MetaLen, writePerm)
@@ -191,14 +193,14 @@ func (s *Decoded) SerializeTo(b []byte /*@, buf []byte, dataLen int @*/) (r erro
 	//@ ghost slices.Unslice_Bytes(b, 0, MetaLen, writePerm)
 	//@ ghost slices.CombineAtIndex_Bytes(b, 0, len(b), MetaLen, writePerm)
 	offset := MetaLen
-	//@ assume len(b) >= MetaLen + s.NumINF * path.InfoLen + s.NumHops * path.HopLen
+	//@ assert unfolding acc(s.Mem(), _) in unfolding acc(s.Base.Mem(), _) in (len(b) >= MetaLen + s.NumINF * path.InfoLen + s.NumHops * path.HopLen)
+	//@ assert unfolding acc(s.Mem(), _) in (s.Base.Len() == unfolding acc(s.Base.Mem(), _) in (MetaLen + s.NumINF * path.InfoLen + s.NumHops * path.HopLen))
+	//@ assert s.Len() == unfolding acc(s.Mem(), _) in s.Base.Len()
 	//@ assert len(b) >= MetaLen + s.NumINF * path.InfoLen + s.NumHops * path.HopLen
 	//@ ghost slices.SplitByIndex_Bytes(b, 0, len(b), offset, writePerm)
-
 	//@ assume 0 < s.NumINF && 0 < s.NumHops // TODO FIXME
 	//@ fold acc(s.Base.Mem(), definitions.ReadL1)
 	//@ fold acc(s.NoBufMem(buf), definitions.ReadL1)
-
 	//@ invariant acc(s.NoBufMem(buf), definitions.ReadL1)
 	//@ invariant slices.AbsSlice_Bytes(b, 0, offset)
 	//@ invariant slices.AbsSlice_Bytes(b, offset, len(b))
