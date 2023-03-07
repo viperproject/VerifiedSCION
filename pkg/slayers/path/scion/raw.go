9f47a502a3f14294296c243ba2909fd7280e2b9f
Dspil <dennisspiliopoylos@gmail.com>
2023-03-07 13:38:35 +0100

    stuff

--- pkg/slayers/path/scion/raw.go
+++ pkg/slayers/path/scion/raw.go
@@ -129,11 +129,12 @@ func (s *Raw) Reverse( /*@ ghost ubuf []byte @*/ ) (p path.Path, err error) {
 }
 
 // ToDecoded transforms a scion.Raw to a scion.Decoded.
+// @ requires  slices.AbsSlice_Bytes(ubuf, 0, len(ubuf))
 // @ requires  s.Mem(ubuf)
-// @ preserves slices.AbsSlice_Bytes(ubuf, 0, len(ubuf))
+// @ ensures   slices.AbsSlice_Bytes(ubuf, 0, len(ubuf))
 // @ ensures   s.Mem(ubuf)
 // @ ensures   err == nil ==> d.Mem(unfolding acc(s.Mem(ubuf), _) in s.Raw)
-// @ ensures   err == nil ==> (old(unfolding s.Mem(ubuf) in s.Base.InfValid()) ==> unfolding d.Mem(unfolding acc(s.Mem(ubuf), _) in s.Raw) in d.Base.InfValid())
+// @ ensures   err == nil ==> (old(s.InfValid(ubuf)) ==> d.InfValid(unfolding acc(s.Mem(ubuf), _) in s.Raw))
 // @ ensures   err != nil ==> err.ErrorMem()
 // @ decreases
 func (s *Raw) ToDecoded( /*@ ghost ubuf []byte @*/ ) (d *Decoded, err error) {
