1ada8bdcd7d4eb58384a3c53398a3df9e628dd12
João Pereira <joaopereira.19@gmail.com>
2023-04-13 17:14:49 +0200

    uncomment initSpec

--- router/dataplane.go
+++ router/dataplane.go
@@ -15,17 +15,17 @@
 // +gobra
 
 // (VerifiedSCION) Uncommenting the following causes severe slowdowns, but it verifies
-// initEnsures alreadySet                    != nil && alreadySet.ErrorMem()
-// initEnsures cannotRoute                   != nil && cannotRoute.ErrorMem()
-// initEnsures emptyValue                    != nil && emptyValue.ErrorMem()
-// initEnsures malformedPath                 != nil && malformedPath.ErrorMem()
-// initEnsures modifyExisting                != nil && modifyExisting.ErrorMem()
-// initEnsures noSVCBackend                  != nil && noSVCBackend.ErrorMem()
-// initEnsures unsupportedPathType           != nil && unsupportedPathType.ErrorMem()
-// initEnsures unsupportedPathTypeNextHeader != nil && unsupportedPathTypeNextHeader.ErrorMem()
-// initEnsures noBFDSessionFound             != nil && noBFDSessionFound.ErrorMem()
-// initEnsures noBFDSessionConfigured        != nil && noBFDSessionConfigured.ErrorMem()
-// initEnsures errBFDDisabled                != nil && errBFDDisabled.ErrorMem()
+// @ initEnsures alreadySet                    != nil && alreadySet.ErrorMem()
+// @ initEnsures cannotRoute                   != nil && cannotRoute.ErrorMem()
+// @ initEnsures emptyValue                    != nil && emptyValue.ErrorMem()
+// @ initEnsures malformedPath                 != nil && malformedPath.ErrorMem()
+// @ initEnsures modifyExisting                != nil && modifyExisting.ErrorMem()
+// @ initEnsures noSVCBackend                  != nil && noSVCBackend.ErrorMem()
+// @ initEnsures unsupportedPathType           != nil && unsupportedPathType.ErrorMem()
+// @ initEnsures unsupportedPathTypeNextHeader != nil && unsupportedPathTypeNextHeader.ErrorMem()
+// @ initEnsures noBFDSessionFound             != nil && noBFDSessionFound.ErrorMem()
+// @ initEnsures noBFDSessionConfigured        != nil && noBFDSessionConfigured.ErrorMem()
+// @ initEnsures errBFDDisabled                != nil && errBFDDisabled.ErrorMem()
 package router
 
 import (
