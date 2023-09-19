f6625b6b2514a9f86d0f3e29969f504ec195a59c
João Pereira <joaopereira.19@gmail.com>
2023-09-20 00:35:24 +0200

    add intermediate assertions to proof

--- pkg/slayers/scion.go
+++ pkg/slayers/scion.go
@@ -807,6 +807,12 @@ func (s *SCION) AddrHdrLen( /*@ ghost ubuf []byte, ghost insideSlayers bool @*/ 
 		defer fold acc(s.Mem(ubuf), def.ReadL20/2)
 		unfold acc(s.HeaderMem(ubuf[CmnHdrLen:]), def.ReadL20/2)
 		defer fold acc(s.HeaderMem(ubuf[CmnHdrLen:]), def.ReadL20/2)
+		assert s.AddrHdrLenSpec(ubuf) == (
+			unfolding acc(s.Mem(ubuf), _) in
+			unfolding acc(s.HeaderMem(ubuf[CmnHdrLen:]), _) in
+			2*addr.IABytes + s.DstAddrType.Length() + s.SrcAddrType.Length())
+		assert s.AddrHdrLenSpec(ubuf) ==
+			2*addr.IABytes + s.DstAddrType.Length() + s.SrcAddrType.Length()
 	}
 	@*/
 	return 2*addr.IABytes + s.DstAddrType.Length() + s.SrcAddrType.Length()
