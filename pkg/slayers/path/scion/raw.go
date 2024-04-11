9e479386387f25827158e9c32594edc039eb9db1
Dspil <dennisspiliopoylos@gmail.com>
2024-04-11 17:34:08 +0200

    stuff

--- pkg/slayers/path/scion/raw.go
+++ pkg/slayers/path/scion/raw.go
@@ -385,7 +385,7 @@ func (s *Raw) SetInfoField(info path.InfoField, idx int /*@, ghost ubuf []byte, 
 // @ preserves acc(sl.AbsSlice_Bytes(ubuf, 0, len(ubuf)), R10)
 // @ ensures   (idx < old(s.GetNumHops(ubuf))) == (r == nil)
 // @ ensures   r != nil ==> r.ErrorMem()
-// @ ensures   hop.CorrectlyDecodedHF(s.Raw, hopOffset, hopOffset, hopOffset+path.HopLen)
+// @ ensures   r == nil ==> unfolding acc(s.Mem(ubuf), R30) in unfolding acc(s.Base.Mem(), R30) in s.CorrectlyDecodedHF_Raw(res, MetaLen + s.NumINF*path.InfoLen + idx*path.HopLen, MetaLen + s.NumINF*path.InfoLen + idx*path.HopLen, MetaLen + s.NumINF*path.InfoLen + idx*path.HopLen + path.HopLen, ubuf)
 // @ decreases
 func (s *Raw) GetHopField(idx int /*@, ghost ubuf []byte @*/) (res path.HopField, r error) {
 	// @ unfold acc(s.Mem(ubuf), R10)
@@ -400,16 +400,23 @@ func (s *Raw) GetHopField(idx int /*@, ghost ubuf []byte @*/) (res path.HopField
 	// @ fold acc(s.Base.Mem(), R11)
 	// @ fold acc(s.Mem(ubuf), R10)
 	hop /*@@@*/ := path.HopField{}
-	// @ s.RawRangePerm(ubuf, hopOffset, hopOffset+path.HopLen, R10)
+	// @ s.RawRangePerm(ubuf, hopOffset, hopOffset+path.HopLen, R11)
 	if err := hop.DecodeFromBytes(s.Raw[hopOffset : hopOffset+path.HopLen]); err != nil {
 		// @ Unreachable()
 		return path.HopField{}, err
 	}
+	// @ sl.Unslice_Bytes(s.Raw, hopOffset, hopOffset+path.HopLen, R30)
 	// @ assert hop.CorrectlyDecodedHF(s.Raw[hopOffset : hopOffset+path.HopLen], 0, 0, path.HopLen)
-	// @ CorrectlDecodedHF_Lemma(s.Raw, hopOffset, hopOffset+path.HopLen, 0, 0, path.HopLen)
+	// @ hop.CorrectlyDecodedHF_Lemma(s.Raw, hopOffset, hopOffset+path.HopLen, 0, 0, path.HopLen)
 	// @ assert hop.CorrectlyDecodedHF(s.Raw, hopOffset, hopOffset, hopOffset+path.HopLen)
-	// @ s.UndoRawRangePerm(ubuf, hopOffset, hopOffset+path.HopLen, R10)
+	// @ unfold acc(s.Mem(ubuf), R30)
+	// @ hop.CorrectlyDecodedHF_WidenLemma(s.Raw, hopOffset, hopOffset, hopOffset+path.HopLen, ubuf)
+	// @ sl.Reslice_Bytes(s.Raw, hopOffset, hopOffset+path.HopLen, R30)
+	// @ s.UndoRawRangePerm(ubuf, hopOffset, hopOffset+path.HopLen, R11)
 	// @ unfold hop.Mem()
+	// @ assert path.CorrectlyDecodedHF_FullBuf(hop, ubuf, hopOffset, hopOffset, hopOffset+path.HopLen)
+	// @ fold acc(s.Mem(ubuf), R30)
+	// @ assert reveal s.CorrectlyDecodedHF_Raw(hop, hopOffset, hopOffset, hopOffset+path.HopLen, ubuf)
 	return hop, nil
 }
 
