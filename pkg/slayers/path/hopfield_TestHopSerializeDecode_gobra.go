2c9faac28ce23be79a87a767a961d7ca9464773c
Lea Salome Brugger <leasalome.brugger@inf.ethz.ch>
2025-06-10 09:38:16 +0200

    Fix tests in slayers/path

--- pkg/slayers/path/hopfield_TestHopSerializeDecode_gobra.go
+++ pkg/slayers/path/hopfield_TestHopSerializeDecode_gobra.go
@@ -1,34 +1,39 @@
 package path
 
+import (
+	// @ . "github.com/scionproto/scion/verification/utils/definitions"
+	// @ sl "github.com/scionproto/scion/verification/utils/slices"
+	"fmt"
+	"github.com/scionproto/scion/assertion"
+)
+
 func TestHopSerializeDecode(h_HopField_SerializeTo *HopField, b_HopField_SerializeTo []byte, h_HopField_DecodeFromBytes *HopField, raw_HopField_DecodeFromBytes []byte) {
 	var tmp_h_HopField_SerializeTo *HopField
 	var tmp_b_HopField_SerializeTo []byte
 	var tmp_h_HopField_DecodeFromBytes *HopField
 	var tmp_raw_HopField_DecodeFromBytes []byte
-	tmp_h_HopField_SerializeTo = &HopField{IngressRouterAlert: true, EgressRouterAlert: true, ExpTime: 0x3f, ConsIngress: 0x1, ConsEgress: 0x0, Mac: [6]uint8{0x1, 0x2, 0x3, 0x4, 0x5, 0x6}}
+	tmp_h_HopField_SerializeTo = &HopField{IngressRouterAlert: true, EgressRouterAlert: true, ExpTime: 0x3f, ConsIngress: 0x1, ConsEgress: 0x0, Mac: [6]byte{0x1, 0x2, 0x3, 0x4, 0x5, 0x6}}
 	tmp_b_HopField_SerializeTo = []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
-	tmp_h_HopField_DecodeFromBytes = &HopField{IngressRouterAlert: false, EgressRouterAlert: false, ExpTime: 0x0, ConsIngress: 0x0, ConsEgress: 0x0, Mac: [6]uint8{0x0, 0x0, 0x0, 0x0, 0x0, 0x0}}
+	tmp_h_HopField_DecodeFromBytes = &HopField{IngressRouterAlert: false, EgressRouterAlert: false, ExpTime: 0x0, ConsIngress: 0x0, ConsEgress: 0x0, Mac: [6]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0}}
 	tmp_raw_HopField_DecodeFromBytes = []byte{0x3, 0x3f, 0x0, 0x1, 0x0, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6}
 	// @ inhale  len(b_HopField_SerializeTo) >= HopLen
 	// @ inhale acc(h_HopField_SerializeTo.Mem(), R10)
 	// @ inhale sl.Bytes(b_HopField_SerializeTo, 0, HopLen)
+	// @ exhale acc(tmp_h_HopField_SerializeTo)
 	// @ assume h_HopField_SerializeTo == tmp_h_HopField_SerializeTo
 	// @ assume b_HopField_SerializeTo === tmp_b_HopField_SerializeTo
-	// @ assume h_HopField_DecodeFromBytes == tmp_h_HopField_DecodeFromBytes
-	// @ assume raw_HopField_DecodeFromBytes === tmp_raw_HopField_DecodeFromBytes
 	// @ refute false
-	err := tmp_h_HopField_SerializeTo.SerializeTo(tmp_b_HopField_SerializeTo)
-	// @ assert err == nil
+	err := h_HopField_SerializeTo.SerializeTo(b_HopField_SerializeTo)
+	// @ assert unfolding acc(h_HopField_SerializeTo.Mem(), R10) in err == nil
 
 	// @ inhale  acc(h_HopField_DecodeFromBytes)
 	// @ inhale  len(raw_HopField_DecodeFromBytes) >= HopLen
 	// @ inhale acc(sl.Bytes(raw_HopField_DecodeFromBytes, 0, HopLen), R45)
-	// @ assume h_HopField_SerializeTo == tmp_h_HopField_SerializeTo
-	// @ assume b_HopField_SerializeTo === tmp_b_HopField_SerializeTo
+	// @ exhale acc(tmp_h_HopField_DecodeFromBytes)
 	// @ assume h_HopField_DecodeFromBytes == tmp_h_HopField_DecodeFromBytes
 	// @ assume raw_HopField_DecodeFromBytes === tmp_raw_HopField_DecodeFromBytes
 	// @ refute false
-	err := tmp_h_HopField_DecodeFromBytes.DecodeFromBytes(tmp_b_HopField_SerializeTo)
-	// @ assert err == nil
-	// @ assert *HopField{IngressRouterAlert:true, EgressRouterAlert:true, ExpTime:0x3f, ConsIngress:0x1, ConsEgress:0x0, Mac:[6]uint8{0x1, 0x2, 0x3, 0x4, 0x5, 0x6}} == tmp_h_HopField_DecodeFromBytes
+	err := h_HopField_DecodeFromBytes.DecodeFromBytes(b_HopField_SerializeTo)
+	// @ assert unfolding h_HopField_DecodeFromBytes.Mem() in err == nil
+	// @ assert unfolding h_HopField_DecodeFromBytes.Mem() in HopField{IngressRouterAlert:true, EgressRouterAlert:true, ExpTime:0x3f, ConsIngress:0x1, ConsEgress:0x0, Mac:[6]byte{0x1, 0x2, 0x3, 0x4, 0x5, 0x6}} == *h_HopField_DecodeFromBytes
 }
--- pkg/slayers/path/infofield_TestInfoSerializeDecode_gobra.go
+++ pkg/slayers/path/infofield_TestInfoSerializeDecode_gobra.go
@@ -1,5 +1,12 @@
 package path
 
+import (
+	// @ . "github.com/scionproto/scion/verification/utils/definitions"
+	// @ sl "github.com/scionproto/scion/verification/utils/slices"
+	"fmt"
+	"github.com/scionproto/scion/assertion"
+)
+
 func TestInfoSerializeDecode(inf_InfoField_SerializeTo *InfoField, b_InfoField_SerializeTo []byte, inf_InfoField_DecodeFromBytes *InfoField, raw_InfoField_DecodeFromBytes []byte) {
 	var tmp_inf_InfoField_SerializeTo *InfoField
 	var tmp_b_InfoField_SerializeTo []byte
@@ -11,24 +18,22 @@ func TestInfoSerializeDecode(inf_InfoField_SerializeTo *InfoField, b_InfoField_S
 	tmp_raw_InfoField_DecodeFromBytes = []byte{0x3, 0x0, 0x2, 0x22, 0x0, 0x0, 0x1, 0x0}
 	// @ inhale  len(b_InfoField_SerializeTo) >= InfoLen
 	// @ inhale acc(inf_InfoField_SerializeTo, R10)
-	// @ inhale slices.Bytes(b_InfoField_SerializeTo, 0, len(b_InfoField_SerializeTo))
+	// @ inhale sl.Bytes(b_InfoField_SerializeTo, 0, len(b_InfoField_SerializeTo))
+	// @ exhale acc(tmp_inf_InfoField_SerializeTo)
 	// @ assume inf_InfoField_SerializeTo == tmp_inf_InfoField_SerializeTo
 	// @ assume b_InfoField_SerializeTo === tmp_b_InfoField_SerializeTo
-	// @ assume inf_InfoField_DecodeFromBytes == tmp_inf_InfoField_DecodeFromBytes
-	// @ assume raw_InfoField_DecodeFromBytes === tmp_raw_InfoField_DecodeFromBytes
 	// @ refute false
-	err := tmp_inf_InfoField_SerializeTo.SerializeTo(tmp_b_InfoField_SerializeTo)
-	// @ assert err == nil
+	err := inf_InfoField_SerializeTo.SerializeTo(b_InfoField_SerializeTo)
+	// @ assert unfolding sl.Bytes(b_InfoField_SerializeTo, 0, len(b_InfoField_SerializeTo)) in err == nil
 
 	// @ inhale  len(raw_InfoField_DecodeFromBytes) >= InfoLen
 	// @ inhale acc(inf_InfoField_DecodeFromBytes)
-	// @ inhale acc(slices.Bytes(raw_InfoField_DecodeFromBytes, 0, len(raw_InfoField_DecodeFromBytes)), R45)
-	// @ assume inf_InfoField_SerializeTo == tmp_inf_InfoField_SerializeTo
-	// @ assume b_InfoField_SerializeTo === tmp_b_InfoField_SerializeTo
+	// @ inhale acc(sl.Bytes(raw_InfoField_DecodeFromBytes, 0, len(raw_InfoField_DecodeFromBytes)), R45)
+	// @ exhale acc(tmp_inf_InfoField_DecodeFromBytes)
 	// @ assume inf_InfoField_DecodeFromBytes == tmp_inf_InfoField_DecodeFromBytes
 	// @ assume raw_InfoField_DecodeFromBytes === tmp_raw_InfoField_DecodeFromBytes
 	// @ refute false
-	err := tmp_inf_InfoField_DecodeFromBytes.DecodeFromBytes(tmp_b_InfoField_SerializeTo)
+	err := inf_InfoField_DecodeFromBytes.DecodeFromBytes(b_InfoField_SerializeTo)
 	// @ assert err == nil
-	// @ assert *InfoField{Peer:true, ConsDir:true, SegID:0x222, Timestamp:0x100} == tmp_inf_InfoField_DecodeFromBytes
+	// @ assert unfolding acc(sl.Bytes(b_InfoField_SerializeTo, 0, len(b_InfoField_SerializeTo)), R45) in InfoField{Peer:true, ConsDir:true, SegID:0x222, Timestamp:0x100} == *inf_InfoField_DecodeFromBytes
 }
